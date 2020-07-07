85a9a17f39278ebcad2952de70c00641bd47916b
finfinley <alexfin@outlook.com>
2020-07-06 21:05:03 -0400

    more in depth comments

--- Arrays101/RemoveElement.py
+++ Arrays101/RemoveElement.py
@@ -1,6 +1,7 @@
 nums = [0,1,2,2,3,0,4,2]
 val = 2
-print(nums.count(val))
-while nums.count(val): # count method returns the number of elements with the specified value
-    nums.remove(val)
+
+# count method returns the number of elements with the specified value
+while nums.count(val): # will loop through nums while val is in nums
+    nums.remove(val) # removes val from nums while val is in nums (while loop, see above)
 print(nums)
